7e05e4b8b01e7c9961f55c8438e1547d49ebeaae
jahid khan <jahidkhan0057@gmail.com>
2021-03-05 09:47:23 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+$('.banner-area .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayspeed:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+$('.about-area .owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    autoplay:true,
+    dots:false,
+    autoplayspeed:1000,    
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+
+$('.logo-area .owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    autoplay:true,
+    autoplayspeed:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
